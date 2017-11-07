fcbbee10706cec03a5a05150a75de6f7531e5bcc
Abirkulov Sherali <abirkulovs@gmail.com>
2017-11-07 20:52:39 +0300

    Commit from 07.11.2017. Finishing with styling sidebar.

--- js/main.js
+++ js/main.js
@@ -8,4 +8,15 @@ $(function(){
         $(".main-menu .navbar-form").slideToggle();
     });
 
+    $(window).load(function(){
+        var carouselCaptionWidth = $("#carousel-sidebar .active img").width();
+        $("#carousel-sidebar img").each(function(){
+            $(this).attr('width', carouselCaptionWidth);
+        });
+
+        $("#carousel-sidebar .sidebar-carousel-caption").css("max-width", carouselCaptionWidth);
+        $("#carousel-sidebar .carousel-indicators").css("max-width", carouselCaptionWidth);
+        $(".sidebar .banner").css("max-width", carouselCaptionWidth);
+    });
+
 })
